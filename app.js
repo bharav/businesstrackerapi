de0e7fc91428a452a300199cf4e66b33e5a29d61
bharav <vivek.bharatia@emc.com>
2015-10-20 10:52:12 +0530

    Initial commit for app js

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Created by vivek on 19-10-2015.
+ */
+var express = require('express'),
+    mongoose=require('mongoose'),
+    bodyParser = require('body-parser');
+
+var db = mongoose.connect('mongodb://localhost/businesstrackerApi');
+var Order = require('./models/orderModel')
+
+var app = express();
+var port = process.env.PORT || 3000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+var orderRouter = express.Router();
+
+orderRouter.route('/Orders')
+    .post(function (req,res) {
+        var order = new Order(req.body);
+       order.save();
+        res.status(201).send(order);
+
+    })
+    .get(function(req,res){
+        Order.find(function (err,orders) {
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(orders);
+        })
+    });
+
+app.use('/api', orderRouter);
+
+app.get('/', function (req,res) {
+    res.send('Welcome to business tracker api');
+});
+
+app.listen(port, function () {
+    console.log('Gulp is running our app on PORT '+ port);
+} )
---
+++ models/orderModel.js
@@ -0,0 +1,29 @@
+/**
+ * Created by vivek on 19-10-2015.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var product = new Schema({
+    productid:{type:String},
+    productname:{type:String},
+    productunit:{type:Number},
+    productunitprice:{type:Number}
+});
+var orderModel = new Schema({
+    orderdate:{type:Date, default:Date.now},
+    custname:{type:String},
+    custaddress:{type:String},
+    custphone:{type:Number},
+    product:[product],
+    paymentstatus:{type:Boolean, default:false},
+    paymenttype:{type:String},
+    paymentBank:{type:String},
+    paymentWallet:{type:String},
+    paymentcomment:{type:String},
+    orderstatus:{type:String},
+    ordershipping:{type:String},
+    orderstatuscomment:{type:String}
+});
+
+module.exports=mongoose.model('Order',orderModel);
